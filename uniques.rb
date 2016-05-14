2473dbb000c4ccf655d78f7ffe35b1e195cbfe27
Graham Turner <turnertgraham@gmail.com>
2016-05-14 00:03:04 -0400

    completed uniques method

---
+++ uniques.rb
@@ -0,0 +1,13 @@
+def uniques(arr)
+	
+	arr.length.times do |i|
+		arr.length.times do |k|
+			if arr[i] == arr[k] && i != k
+				arr.delete_at k
+			end
+		end
+	end
+	puts arr
+end
+
+uniques [1, 5 ,"frog", 2 ,1 , 3, "frog"]
